1677422930405ee1973060013ed0a204c2471024
Vadim Kuznetsov <vadimkuznetsov1132@yandex.ru>
2026-05-05 01:09:27 +0300

    add react-hook-form component

--- frontend/client/src/components/Login/Login.tsx
+++ frontend/client/src/components/Login/Login.tsx
@@ -1,11 +1,21 @@
-import { Container } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return(
-        <Container>
-            <h1>Логин</h1>
-        </Container>
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm()
+    return (
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
+            <input {...register('firstName')} />
+            <input {...register('lastName', { required: true })} />
+            {errors.lastName && <p>Второе имя обязательно</p>}
+            <input {...register('age', { pattern: /\d+/ })} />
+            {errors.age && <p>Please enter number for age.</p>}
+            <input type="submit" />
+        </form>
     )
 }
-
 export default Login
