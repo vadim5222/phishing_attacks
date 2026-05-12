7f21a15d19f781654d6f3d4bf22d310daea025e7
Vadim Kuznetsov <vadimkuznetsov1132@yandex.ru>
2026-05-12 18:51:51 +0300

    fix frontend

--- frontend/client/src/components/UserProfilleUpdateForm/UserProfileUpdateForm.jsx
+++ frontend/client/src/components/UserProfilleUpdateForm/UserProfileUpdateForm.jsx
@@ -5,14 +5,12 @@ import { useState } from "react"
 const UserProfileUpdateForm = ({userId}) => {
 
     const {register, handleSubmit} = useForm()
-    const [selectedImage, setSelectedImage] = useState(null)
 
     const onEdit = async(data) => {
         try{
             const formData = new FormData()
             formData.append('username', data.username)
             formData.append('email', data.email)
-            formData.append('image', selectedImage)
             const response = await AxiosRequest.put(`accounts/profile/${userId}/`,
                 formData
             )
@@ -29,7 +27,6 @@ const UserProfileUpdateForm = ({userId}) => {
                 <div>
                     <input type="text" {...register('username')}/>
                     <input type="email" {...register('email')}/>
-                    <input type="file" onChange={(e) => setSelectedImage(e.target.files[0])}/>
                     <button type="submit">Сохранить</button>
                 </div>
             </form>
