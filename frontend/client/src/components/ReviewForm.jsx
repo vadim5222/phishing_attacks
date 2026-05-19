6f32885ec3d3c50ee011d4e8e9e7199973316f50
Vadim Kuznetsov <vadimkuznetsov1132@yandex.ru>
2026-05-19 19:08:59 +0300

    adding reviews page

--- frontend/client/src/components/ReviewForm.jsx
+++ frontend/client/src/components/ReviewForm.jsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form'
+import { useState } from 'react'
 import AxiosRequest from './AxiosRequest'
 
 const ReviewForm = () => {
@@ -20,14 +21,28 @@ const ReviewForm = () => {
         }
     }
 
+
+
+
     return (
-        <>
-            <form onSubmit={handleSubmit(createReview)}>
-                <input type="text" {...register('text')} />
-                <input type="number" {...register('score')} />
-                <button type='submit'>Отправить</button>
-            </form>
-        </>
+        <div>
+            <div className='flex justify-center'>
+                <form className='p-4 md:p-6 lg:p-8 bg-cyan-950 w-3/6 rounded-xl mb-10' onSubmit={handleSubmit(createReview)}>
+                    <label className='block mb-1' htmlFor="text">Отзыв</label>
+                    <textarea
+                        className='w-full h-52 p-4 border text-3xl border-gray-300 rounded-md focus:outline-none mb-3 focus:ring-blue-500 focus:border-blue-500' type="text"
+                        placeholder='Введите ваш отзыв'
+                        {...register('text')} />
+                    <label className='block mb-1' htmlFor="score">Оценка</label>
+                    <input
+                        className='w-52 block px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500'
+                        type="number"
+                        placeholder='Поставьте оценку'
+                        {...register('score')} />
+                    <button className='w-56 flex mx-auto justify-center my-5 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 hover:bg-slate-800' type='submit'>Отправить</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
--- frontend/client/src/pages/ReviewsPage.jsx
+++ frontend/client/src/pages/ReviewsPage.jsx
@@ -1,14 +1,36 @@
 import Header from "../components/Header"
 import ReviewForm from "../components/ReviewForm"
 import Footer from '../components/Footer'
+import { useState, useEffect } from "react"
+import AxiosRequest from "../components/AxiosRequest"
 
 const ReviewsPage = () => {
+
+    const [review, setReview] = useState([])
+
+    useEffect(() => {
+        AxiosRequest.get('accounts/review/', {
+            withCredentials: true
+        })
+        .then(res => setReview(res.data))
+        .catch(() => setReview([]))
+    }, [])
+
     return (
         <div className="min-h-screen flex flex-col">
             <Header />
-            <div className="flex-1">
-                <ReviewForm />
-            </div>
+                <div className="container mx-auto flex-1 ">
+                    <ReviewForm />
+                <div className="flex flex-col  items-center">
+                    {review.map((review) => 
+                    <div key={review.id} className="bg-slate-600 mb-5 w-1/3 p-3 rounded-lg">
+                        <p>{review.user}</p>
+                        <p>{review.text}</p>
+                        <p>{review.score}</p>
+                    </div>
+                    )}
+                </div>
+                </div>
             <Footer />
         </div>
     )
